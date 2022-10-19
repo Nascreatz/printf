2eab2db553180ad3686c1d2b55711f13de928777
Nascreat <nascreatz16@gmail.com>
2022-10-19 14:40:58 -0700

    Some advanced added

--- _putchar.c
+++ _putchar.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
- *
- * Return:( str-a)
- */
-int _puts(char *str)
-{
-	char *a = str;/*declaration of variables*/
-
-	while (*str)
-		_putchar(*str++);
-	return (str - a);
-}
-
-/**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(int c)
+int _putchar(char c)
 {
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
+	return (buffer(c));
+}
 
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+/**
+ * buffer - Save the character in a buffer
+ * @c: Character
+ *
+ * Return: 1
+ **/
+int buffer(char c)
+{
+	static char buffering[1024];
+	static int i;
+
+	if (c == -1 || i == 1024)
 	{
-		write(1, buf, i);
+		write(1, buffering, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+
+	if (c != -1)
+		buffering[i++] = c;
+
 	return (1);
 }
